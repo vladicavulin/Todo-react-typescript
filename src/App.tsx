87b4e5a67ca310529c25a3f9a7622a64831d7ff3
vladica <vladicavulin@gmail.com>
2020-06-09 11:13:22 +0200

    The TodoList is up and working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TodoListItem } from './TodoListItem' 
+import { TodoList } from './TodoList' 
 
 const initialTodos: Array <Todo> = [
   { text:"Walk the dog", complete:true },
@@ -24,8 +24,7 @@ const toggleTodo: ToggleTodo = selectedTodo => {
 
   return (
   <React.Fragment>
-  <TodoListItem todo={todos[0]} toggleTodo={toggleTodo}/>
-  <TodoListItem todo={todos[1]} toggleTodo={toggleTodo}/>
+  <TodoList todos={todos} toggleTodo={toggleTodo}/>
   </React.Fragment>
   )
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { TodoListItem } from './TodoListItem';
 
-export const TodoList = ({ todos, toggleTodo }) => {
+interface TodoListProps {
+    todos: Array<Todo>;
+    toggleTodo: ToggleTodo;
+}
+
+export const TodoList: React.FC <TodoListProps> = ({ todos, toggleTodo }) => {
     return (
         <ul>
         {todos.map(todo => {
-            return <TodoListItem todo={todo} toggleTodo={toggleTodo} />
+            return <TodoListItem key={todo.text} todo={todo} toggleTodo={toggleTodo} />
         })}
         </ul>
-  ;
+    );
 }
