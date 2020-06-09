303174fa4a61e53b6445b1bb37316c90b605ee81
vladica <vladicavulin@gmail.com>
2020-06-09 10:58:43 +0200

    The toggleTodo method is done and working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoListItem } from './TodoListItem' 
 
-const todos: Array <Todo> = [
+const initialTodos: Array <Todo> = [
   { text:"Walk the dog", complete:true },
   { text:"Write an app", complete: false }
 ];
 
 export const App: React.FC = () => {
+const [todos, setTodos] = useState(initialTodos)
+
+const toggleTodo: ToggleTodo = selectedTodo => {
+  const newTodos = todos.map(todo => {
+    if(todo === selectedTodo) {
+      return {
+        ...todo,
+        complete: !todo.complete
+      };
+    }
+    return todo;
+  });
+  setTodos(newTodos);
+}
+
   return (
   <React.Fragment>
-  <TodoListItem todo={todos[0]}/>
-  <TodoListItem todo={todos[1]}/>
+  <TodoListItem todo={todos[0]} toggleTodo={toggleTodo}/>
+  <TodoListItem todo={todos[1]} toggleTodo={toggleTodo}/>
   </React.Fragment>
   )
 }
